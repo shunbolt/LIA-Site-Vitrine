3170ab1ce170564a120f7903262107a794e4ea38
Gaullier <gaullier.theo@gmail.com>
2020-04-14 20:56:27 +0200

    Update contact.html
    Ajout de mailto pour l'envoie de mail

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-#def application(env, start_response):
-#    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
-#    return ['<!DOCTYPE html><html><meta charset="utf-8"><title>It works'.encode('utf-8'),
-#            "</title><b>It works!</b><br /><br />This is the server's ".encode('utf-8'),
-#            'default wsgi.py.'.encode('utf-8')]
-
-from flask_chat import app as application
